b3b2a7749f7d8078a279bf4aee0dce06843eb35a
Sigurd Spieckermann <sigurd.spieckermann@gmail.com>
2024-10-15 09:40:12 +0200

    build: drop support for Python 3.7

--- tests/test/test_local.py
+++ tests/test/test_local.py
@@ -1,8 +1,8 @@
 """Tests for using local schema files."""
 
-import sys
 from pathlib import Path
 from typing import Any
+from typing import Literal
 
 import pytest
 from jinja2 import Environment
@@ -16,11 +16,6 @@ from tests.utils import create_env
 from tests.utils import serialize
 
 from .utils import TEST_CASES
-
-if sys.version_info >= (3, 8):  # pragma: no cover
-    from typing import Literal
-else:  # pragma: no cover
-    from typing_extensions import Literal
 
 
 @pytest.mark.parametrize(("data", "message"), TEST_CASES)
